07b8e51100ce453bacd897fa2bc137829e25d4c2
CariP03 <pasqualecaringellab@gmail.com>
2025-06-13 21:16:52 +0200

    feat: add function to query the KB through python
    Add function that queries the KB using pyswip.

---
+++ src/python/prolog_query.py
@@ -0,0 +1,37 @@
+from pyswip import Prolog
+import os
+
+from tokenizer import clean_tokenize
+import config as cfg
+
+prolog = Prolog()
+
+lex_path = os.path.abspath(cfg.LEXICON_PATH).replace('\\', '/')
+list(prolog.query(f"consult('{lex_path}')"))
+
+rule_path = os.path.abspath(cfg.SENTIMENT_PATH).replace('\\', '/')
+list(prolog.query(f"consult('{rule_path}')"))
+
+def compute_metrics_prolog(tokens):
+    # reset token_at
+    list(prolog.query("retractall(token_at(_, _))"))
+
+    # assert all tokens
+    for index, token in enumerate(tokens, start=1):
+        prolog.assertz(f'token_at({index}, "{token}")')
+
+    # query the knowledge base
+    sum_query = list(prolog.query("sentiment_sum(S)"))
+    cnt_query = list(prolog.query("sentiment_count_nonzero(C)"))
+    tot_query = list(prolog.query("sentiment_total_tokens(T)"))
+
+    # retrieve results
+    sentiment_sum = sum_query[0]['S'] if sum_query else 0
+    non_zero_count = cnt_query[0]['C'] if cnt_query else 0
+    num_tokens = tot_query[0]['T'] if tot_query else 0
+
+    # calculate useful metrics
+    avg_nonzero = sentiment_sum / non_zero_count if non_zero_count > 0 else 0
+    ratio = non_zero_count / num_tokens if num_tokens > 0 else 0
+
+    return sentiment_sum, non_zero_count, num_tokens, avg_nonzero, ratio
