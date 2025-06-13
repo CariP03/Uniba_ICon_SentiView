014faf83ab8caab2ae7145ef45f771ed4fee6549
CariP03 <pasqualecaringellab@gmail.com>
2025-06-13 11:30:44 +0200

    feat: add functions to calculate phrases's score
    Add functions that use SentiWordNet to calculate the sentiment score of
    a phrase.

---
+++ src/sentiwordnet_score.py.py
@@ -0,0 +1,30 @@
+from nltk.corpus import sentiwordnet as swn
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import word_tokenize
+
+
+def get_swn_score(word):
+    synsets = wn.synsets(word)
+    scores = []
+
+    for syn in synsets:
+        swn_syn = swn.senti_synset(syn.name())
+        net_score = swn_syn.pos_score() - swn_syn.neg_score()
+        scores.append(net_score)
+
+    if scores:
+        return sum(scores) / len(scores)
+    else:
+        return 0
+
+def get_swn_score_phrase(phrase):
+    tokens = word_tokenize(phrase.lower())
+    scores = []
+
+    for token in tokens:
+        scores.append(get_swn_score(token))
+
+    if scores:
+        return sum(scores) / len(scores)
+    else:
+        return 0
