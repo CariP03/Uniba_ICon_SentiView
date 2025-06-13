2b6234168f3c756d19c8137e3649a95bbaa89c75
CariP03 <pasqualecaringellab@gmail.com>
2025-06-13 17:32:59 +0200

    feat: improved tokenizer
    Add lemmatizer to improve the lexicon. Create a config file to group all
    settings.

---
+++ src/python/config.py
@@ -0,0 +1,23 @@
+DATASET_PATH = "..\\..\\data\\aclImdb\\train\\"
+LEXICON_PATH = "..\\prolog\\lexicon.pl"
+
+# list of intensifiers
+INTENSIFIERS = {
+    "very": 1.5,
+    "extremely": 2.0,
+    "really": 1.3,
+    "so": 1.2,
+    "quite": 1.2,
+    "too": 1.2,
+    "absolutely": 1.7,
+    "highly": 1.5,
+    "incredibly": 1.8,
+    "super": 1.7,
+    "ultra": 1.7
+}
+
+# list of negators
+NEGATORS = {"not", "never", "no", "hardly", "barely"}
+
+THRESHOLD = 0.1
+
--- src/python/tokenizer.py
+++ src/python/tokenizer.py
@@ -1,15 +1,74 @@
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
+import string
 from nltk.corpus import wordnet as wn
+from nltk import word_tokenize, pos_tag
+from nltk.stem import WordNetLemmatizer as wnl
+
+from config import INTENSIFIERS, NEGATORS
 
 def tokenize_dataframe(df):
     vocab = set()
     for text in df["text"]:
-        tokens = word_tokenize(text.lower())
-
-        # remove punctuation
+        tokens = clean_tokenize(text)
         for token in tokens:
-            if any(c.isalpha() for c in token):
-                vocab.add(token)
+            vocab.add(token)
 
     return vocab
+
+
+def clean_tokenize(text: str):
+    def get_wordnet_pos(treebank_tag):
+        # convert NLTK's tags to WordNet's tags
+        if treebank_tag.startswith('J'):
+            return wn.ADJ
+        elif treebank_tag.startswith('V'):
+            return wn.VERB
+        elif treebank_tag.startswith('N'):
+            return wn.NOUN
+        elif treebank_tag.startswith('R'):
+            return wn.ADV
+        else:
+            return wn.NOUN
+
+    # source: https://stackoverflow.com/questions/17245123/getting-adjective-from-an-adverb-in-nltk-or-other-nlp-library
+    # author: alvas (accessed 13 June 2025)
+    def convert_adv_to_adj(adverb):
+        for ss in wn.synsets(adverb, pos=wn.ADV):
+            for lemmas in ss.lemmas():  # all possible lemmas
+                pert = lemmas.pertainyms()  # all possible pertainyms
+                if pert:
+                    return pert[0].name()
+
+        return None
+
+    tokens = word_tokenize(text.lower())
+    pos_tags = pos_tag(tokens)
+    lemmatizer = wnl()
+
+    clean_tokens = []
+    for token, tag in pos_tags:
+        token = token.strip(string.punctuation)
+
+        if any(c.isalpha() for c in token):  # remove invalid tokens
+            if token in NEGATORS:
+                clean_tokens.append(token)
+                continue
+            if token in INTENSIFIERS:
+                clean_tokens.append(token)
+                continue
+
+            pos = get_wordnet_pos(tag)
+            if pos:
+                lemma = lemmatizer.lemmatize(token, pos)
+            else:
+                # if get_wordnet_pos returns None
+                lemma = lemmatizer.lemmatize(token)
+
+            # try to convert adverb to adjective
+            if pos == wn.ADV:
+                adj = convert_adv_to_adj(token)
+                if adj:
+                    lemma = adj
+
+            clean_tokens.append(lemma)
+
+    return clean_tokens
